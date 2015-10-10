a1d7a0506364a0ac5c898100c5fa1d0bd45ebb74
Andreas Johansson <andreas.hf.johansson@gmail.com>
2015-10-10 17:59:33 +0200

    Simple implementation of command responses to initialize arduino

--- arduino-control.js
+++ arduino-control.js
@@ -5,7 +5,12 @@ var serialport = require('serialport');
 
 class ArduinoControl extends events.EventEmitter {
 
-  constructor(serialOptions) {
+  constructor(serialOptions, controlOptions) {
+
+    this.setpoint = controlOptions.setpoint || 19.5;
+    this.kp = controlOptions.kp || 1500;
+    this.ki = controlOptions.ki || 300;
+    this.kd = controlOptions.kd || 50;
 
     this.serial = new serialport.SerialPort(serialOptions.serialPort, {
       baudrate: serialOptions.baudrate || 9600,
@@ -16,11 +21,36 @@ class ArduinoControl extends events.EventEmitter {
       parser: serialport.parsers.readline('\n')
     }, false);
 
+    let self = this;
     this.serial.on('data', function (line) {
       // line is string with utf-8 encoding
 
       var timestamp = new Date();
       console.log(timestamp.toLocaleTimeString() + ' - ' + line);
+
+      // check if incoming data is command from arduino
+      // respond with data if it is
+      //TODO: Should probably wait for the response from the arduino that it actually got the response
+      //TODO: Adding crc checks to lines is probably also a good idea
+      if (line.startsWith('?SP')) {
+
+        let response = 'SP' + self.setpoint.toFixed(1).replace('.', '') + '\n';
+        self.serial.write(response);
+        console.log(' returned setpoint: ' + response);
+
+      } else if (line.startsWith('?PID')) {
+
+        let response = 'PID' + self.kp + ';' + self.ki + ';' + self.kd + '\n';
+        self.serial.write(response);
+        console.log(' returned pid params: ' + response);
+
+      } else if (line.startsWith('?MODE')) {
+
+        let response = 'MODEA\n';
+        self.serial.write(response);
+        console.log(' returned mode auto');
+      }
+
 
     });
 
